4e891aee7b737edc7decb915b38c88a8b9d5023d
Макаренко Мария Дмитриевна <MD.Makarenko@dc.nosu>
2021-12-20 10:38:44 +0300

    Добавьте файлы проекта.

---
+++ Labirint/FormLabirint.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Labirint
+{
+    partial class FormLabirint
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewLab = new System.Windows.Forms.DataGridView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ComboBoxLevels = new System.Windows.Forms.ToolStripComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLab)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewLab
+            // 
+            this.dataGridViewLab.AllowUserToAddRows = false;
+            this.dataGridViewLab.AllowUserToDeleteRows = false;
+            this.dataGridViewLab.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewLab.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewLab.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewLab.Name = "dataGridViewLab";
+            this.dataGridViewLab.ReadOnly = true;
+            this.dataGridViewLab.Size = new System.Drawing.Size(800, 450);
+            this.dataGridViewLab.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ComboBoxLevels});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // ComboBoxLevels
+            // 
+            this.ComboBoxLevels.Items.AddRange(new object[] {
+            "Уровень 1",
+            "Уровень 2"});
+            this.ComboBoxLevels.Name = "ComboBoxLevels";
+            this.ComboBoxLevels.Size = new System.Drawing.Size(121, 25);
+            this.ComboBoxLevels.TextChanged += new System.EventHandler(this.ComboBoxLevels_TextChanged);
+            // 
+            // FormLabirint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.dataGridViewLab);
+            this.Name = "FormLabirint";
+            this.Text = "Лабиринт";
+            this.Load += new System.EventHandler(this.FormLabirint_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewLab)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewLab;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox ComboBoxLevels;
+    }
+}
+
---
+++ Labirint/FormLabirint.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Labirint
+{
+    public partial class FormLabirint : Form
+    {
+        int[,] arrayLab;
+        public FormLabirint()
+        {
+            InitializeComponent();
+        }
+
+        void drawLabirint()
+        {
+            for (int i = 0; i < arrayLab.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayLab.GetLength(1); j++)
+                {
+                    if (arrayLab[i,j] == 1)
+                        dataGridViewLab.Rows[i].Cells[j].Style.BackColor = Color.Red;
+                }
+            }
+            
+        }
+
+        void loadDataGrid(int level)
+        {
+            StreamReader file = new StreamReader("level"+ level.ToString() +".txt");
+            int n = Convert.ToInt32(file.ReadLine());
+            arrayLab = new int[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] temp = file.ReadLine().Split();
+                for (int j = 0; j < n; j++)
+                {
+                    arrayLab[i, j] = Convert.ToInt32(temp[j]);
+                }
+            }
+            file.Close();
+
+            dataGridViewLab.Columns.Clear();
+            dataGridViewLab.Rows.Clear();
+            // добавили n столбцов
+            DataGridViewTextBoxColumn[] columns = new DataGridViewTextBoxColumn[n];
+            for (int i = 0; i < n; i++)
+            {
+                columns[i] = new DataGridViewTextBoxColumn();
+                columns[i].Width = 60;
+            }
+            dataGridViewLab.Columns.AddRange(columns);
+
+            // делаем красиво)
+            dataGridViewLab.RowTemplate.Height = 60;
+            dataGridViewLab.RowHeadersVisible = false;
+            dataGridViewLab.ColumnHeadersVisible = false;
+
+            // добавили n строк
+            dataGridViewLab.Rows.Add(n);
+
+            // установить размер DataGrid
+            dataGridViewLab.Width = n * 60 + 50;
+            dataGridViewLab.Height = n * 60 + 50;
+
+            drawLabirint();
+        }
+
+        private void FormLabirint_Load(object sender, EventArgs e)
+        {
+            loadDataGrid(1);
+        }
+
+        private void ComboBoxLevels_TextChanged(object sender, EventArgs e)
+        {
+            // MessageBox.Show(ComboBoxLevels.Text);
+            int level = Convert.ToInt32(ComboBoxLevels.Text.Split()[1]);
+            loadDataGrid(level);
+        }
+    }
+}
